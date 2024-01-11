f3b733bf0a82e2053f2a26e9d916d0a87a2e2c1c
chetanghodekar7875 <chetanqspider@gmail.com>
2024-01-11 10:55:36 +0530

    Second Commit

--- src/main/java/StudentApplication/WebProject/Controller/StudentController.java
+++ src/main/java/StudentApplication/WebProject/Controller/StudentController.java
@@ -3,7 +3,7 @@ package StudentApplication.WebProject.Controller;
 import StudentApplication.WebProject.Model.Student;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -24,5 +24,49 @@ public class StudentController {
     public String displayStudentData(Model model){
         model.addAttribute("studentData",studentList);
         return "displayStudentData";
+    }
+
+    @GetMapping("/addStudent")
+    public String createObject(Model model){
+        model.addAttribute("studentObject",new Student());
+        return "studentForm";
+    }
+    @PostMapping("/saveStudent")
+    public String saveStudent(Student student){
+        studentList.add(student);
+        return "redirect:/display";
+    }
+
+    @GetMapping("/updateStudent/{id}")
+    public String fetchObject(@PathVariable int id,Model model){
+       for(Student s:studentList){
+           if(s.getStudentRollNumber()==id){
+               model.addAttribute("matchObject",s);
+               break;
+           }
+       }
+       return "updateStudent";
+    }
+
+    @PostMapping("/updateStudentData")
+    public String updateStudent(Student student){
+        for(Student s:studentList){
+            if(s.getStudentRollNumber()==student.getStudentRollNumber()){
+                studentList.set(studentList.indexOf(s),student);
+                break;
+            }
+        }
+        return "redirect:/display";
+    }
+
+    @GetMapping("/deleteStudent/{id}")
+    public String deleteStudent(@PathVariable int id){
+        for(Student s:studentList){
+            if(s.getStudentRollNumber()==id){
+                studentList.remove(s);
+                break;
+            }
+        }
+        return "redirect:/display";
     }
 }
--- src/main/java/StudentApplication/WebProject/Model/Student.java
+++ src/main/java/StudentApplication/WebProject/Model/Student.java
@@ -2,11 +2,13 @@ package StudentApplication.WebProject.Model;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
 @AllArgsConstructor
+@NoArgsConstructor
 public class Student {
     private int studentRollNumber;
     private String studentName;
